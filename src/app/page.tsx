8daf248ba63b6f2e9b309936263b8cdd5134c924
shaheersystems <shaheer.imran1429@gmail.com>
2024-09-22 00:58:41 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main>
+      <div className="mx-auto mt-10 max-w-5xl">
+        <h2 className="text-center text-lg font-semibold">
+          Welcome to our App
+        </h2>
+      </div>
+    </main>
+  );
+}
